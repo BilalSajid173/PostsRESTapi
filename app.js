bc2413def4563beb3c9743a1e852605c89a352e8
Bilal <bsajid173@gmail.com>
2022-06-07 23:23:42 +0530

    Add an error handling middleware

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 app.use(bodyparser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,6 +18,11 @@ app.use((req, res, next) => {
   next();
 });
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  const message = error.message;
+  res.status(error.statusCode).json({ message: message });
+});
 
 mongoose
   .connect("mongodb://localhost:27017/RESTpostsDB")
