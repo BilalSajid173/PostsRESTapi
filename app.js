be0b59237590cdae7e0cb71fa6c59958cce54a50
Bilal <bsajid173@gmail.com>
2022-06-07 10:40:47 +0530

    Configure Get and post requests to our api

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+app.use(bodyparser.json());
+app.use("/feed", feedRoutes);
+app.listen(8080);
