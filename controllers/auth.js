50048479ead8f2ddcfea4f18ca128d4d8afd98be
Bilal <bsajid173@gmail.com>
2022-06-09 13:46:41 +0530

    Logging in using token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -26,6 +27,43 @@ exports.signup = (req, res, next) => {
     })
     .then((result) => {
       res.status(201).json({ message: "User created!", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadeduser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User not found");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadeduser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong Password");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const token = jwt.sign(
+        { email: loadeduser.email, userId: loadeduser._id.toString() },
+        "Messiscored5goalsagainstmightyEstonialol",
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({ token: token, userId: loadeduser._id.toString() });
     })
     .catch((err) => {
       if (!err.statusCode) {
