e4546cbacf864c9914d894ad1cac5d036eb71936
Bilal <bsajid173@gmail.com>
2022-06-09 10:06:29 +0530

    Add User model and routes folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 const mongoose = require("mongoose");
 const path = require("path");
 const multer = require("multer");
@@ -44,6 +45,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   const message = error.message;
